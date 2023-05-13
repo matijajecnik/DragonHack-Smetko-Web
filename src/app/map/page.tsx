28b6cf662eaaaf372403f2f49a1945e625ebc1d1
matijaj <matija.jecnik@gmail.com>
2023-05-13 20:54:07 +0200

    first commit

---
+++ src/app/map/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Card from "@mui/material/Card";
+import React, { useState } from "react";
+
+const Page = () => {
+
+    return (
+        <div>
+            <Card>TODO: map & list of trash</Card>
+        </div>
+    );
+    
+};
+
+export default Page;
---
+++ src/components/foo.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import Link from "next/link";
+
+const Foo = () => {
+
+    return (
+        <div>
+            <Link href="/map">Go To Map</Link>
+            <p>this is foo</p>
+        </div>
+    )
+}
+
+export default Foo;
